86da7f78f21de7c12692f4fa33c4633143fe17fd
Anita Schiller <anita.s1@gmx.de>
2021-06-17 17:01:54 +0200

    Updated imageUrl function

--- client/src/lib/helpers.js
+++ client/src/lib/helpers.js
@@ -3,6 +3,8 @@ export default function imageUrl(imageName) {
   if (absoluteURLPattern.test(imageName)) {
     return imageName;
   } else {
-    return 'http://localhost:4000/assets/' + imageName;
+    return process.env.NODE_ENV === 'development'
+      ? 'http://localhost:4000/assets/' + imageName
+      : '/assets/' + imageName;
   }
 }
